6385dac86ac018035ddb665bddc999fa74cfbc14
李磊 <lilei28@meituan.com>
2016-11-25 11:09:43 +0800

    add some files

---
+++ src/test/java/testcases/SendPost.java
@@ -0,0 +1,17 @@
+package testcases;
+
+import common.HttpRequest;
+import org.testng.annotations.Test;
+
+/**
+ * Created by lilei on 16/11/24.
+ */
+public class SendPost {
+    HttpRequest httpRequest = new HttpRequest();
+
+    @Test
+    public void testCase1() {
+        String s = httpRequest.sendPost("http://10.4.254.66:8414/renderer/wl/quote", "from=PEK&filter=kxmb_ac&to=SHA&queryid=4599da6159e59831&date=2016-12-26&type=multiple&action=noaction");
+        System.out.println(s);
+    }
+}
